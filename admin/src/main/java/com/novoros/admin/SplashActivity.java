b31ea8a50a988bede8fe24e8791ce57d997b3f08
Ali <beingalisoomro@gmail.com>
2022-06-09 14:06:57 -0700

    Refactored

---
+++ admin/src/main/java/com/novoros/admin/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.novoros.admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.novoros.common.R.layout.activity_splash);
+
+
+        Executors.newSingleThreadScheduledExecutor().schedule(() ->{
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        }, 3000, TimeUnit.MILLISECONDS);
+    }
+}
